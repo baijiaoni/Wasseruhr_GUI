d92ed47ce7aebd666cd570904a012d683ec97b4d
baijiaoni <baijiaoni1314@gmail.com>
2017-11-03 15:11:34 +0100

    add Py script msg to Max flow

---
+++ errormsg.py
@@ -0,0 +1,11 @@
+from org.csstudio.opibuilder.scriptUtil import PVUtil
+from org.csstudio.opibuilder.scriptUtil import ConsoleUtil
+from org.eclipse.jface.dialogs import MessageDialog
+from time import gmtime, strftime
+
+a = PVUtil.getDouble(pvs[0])
+
+if a == 30000:
+    MessageDialog.openInformation(
+            None, "Error message", "Maximum flow is 30000");
+
